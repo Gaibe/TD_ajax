eae08a97eb777139e382226786b12044f4e61ab3
haksho <adrien-richard@live.fr>
2015-12-10 10:49:31 +0100

    json

---
+++ scriptJson.php
@@ -0,0 +1,37 @@
+<?php
+
+return 
+[
+	{
+	  "produit": {
+	    "nom" : "produit1",
+	    "prix": 155,
+	    "description": " super produit blablabla"
+	  }
+	}
+	{
+	  "produit": {
+	    "nom" : "ordinateur",
+	    "prix": 1250,
+	    "description": " super produit blablabla"
+	  }
+	}
+	{
+	  "produit": {
+	    "nom" : "ecran",
+	    "prix": 300,
+	    "description": " super produit blablabla"
+	  }
+	}
+	{
+	  "produit": {
+	    "nom" : "café",
+	    "prix": 5,
+	    "description": " super produit blablabla"
+	  }
+	}
+]
+
+
+
+?>
