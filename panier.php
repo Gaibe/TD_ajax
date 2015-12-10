ca164b08644bd25c683eb1307332cc985230096c
haksho <adrien-richard@live.fr>
2015-12-10 11:57:34 +0100

    panier start

---
+++ panier.php
@@ -0,0 +1,36 @@
+<?php
+
+if (isset($_GET )) {
+	echo '
+	[
+	{
+	  "produit": {
+	    "nom" : "PC Gamer",
+	    "prix": 455,
+	    "nombre" : 2,
+	  }
+	},
+	{
+	  "produit": {
+	    "nom" : "Souris",
+	    "prix": 40,
+	    "nombre" : 10,
+	  }
+	},
+	]
+
+
+
+	';
+
+
+
+}
+
+if (isset($_POST )) {
+
+}
+
+
+
+?>
