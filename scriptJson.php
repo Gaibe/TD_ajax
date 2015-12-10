b407431efa4d06d81861be44c918115c8dac2e53
Victorien Schweitzer <victorien-s@live.fr>
2015-12-10 11:51:18 +0100

    Modification des articles

--- scriptJson.php
+++ scriptJson.php
@@ -1,34 +1,34 @@
 <?php
 
 echo '[
-	{
-	  "produit": {
-	    "nom" : "produit1",
-	    "prix": 155,
-	    "description": " super jezhbfjzhebfjzheba"
-	  }
-	},
-	{
-	  "produit": {
-	    "nom" : "ordinateur",
-	    "prix": 1250,
-	    "description": " kzejbcbbbbbbbbbbb"
-	  }
-	},
-	{
-	  "produit": {
-	    "nom" : "ecran",
-	    "prix": 300,
-	    "description": " cccccccccccccccc"
-	  }
-	},
-	{
-	  "produit": {
-	    "nom" : "café",
-	    "prix": 5,
-	    "description": " ccccccddddddddddddddd"
-	  }
-	}
+    {
+      "produit": {
+        "nom" : "Seagate Barracuda 7200.14 SATA 6Gb/s 1 To",
+        "prix": 56.95,
+        "description": "Les disques durs Seagate Barracuda 7200.14 SATA 6Gb/s renferment une puissance inégalée dans un seul disque. Une même plate-forme pour toutes les applications de stockage de bureau. Un disque aux performances éprouvées associant fiabilité, simplicité et capacité."
+      }
+    },
+    {
+      "produit": {
+        "nom" : "Acer Liquid Jade S Noir",
+        "prix": 259.96,
+        "description": "A l\'image de son prédécesseur, le Acer Liquid Jade S bénéficie de superbes lignes arrondies tout en étant aussi compact. Qui plus est, il offre de magnifiques motifs texturés à l\'arrière et des appareils photo à commande vocale, ainsi qu\'une puissance et une vitesse exceptionnelle."
+      }
+    },
+    {
+      "produit": {
+        "nom" : "Sony Playstation 3 Ultra Slim",
+        "prix": 199.95,
+        "description": "Incarnant le meilleur du jeu vidéo et du divertissement à domicile, la PlayStation 3 procure des expériences résolument inédites pour toute la famille. Lancez-vous dans des aventures incroyables, regardez des films en Haute Définition, plongez au coeur d\'un univers de divertissement complet !"
+      }
+    },
+    {
+      "produit": {
+        "nom" : "BenQ 24" LED - GL2460HM",
+        "prix": 159.95,
+        "description": "Le BenQ GL2460HM est un moniteur performant et design, il incarne la simplicité et la sophistication,et est idéal aussi bien pour une utilisation au bureau que chez vous. Il présente chaque détail des images dans la meilleure qualité de visualisation grâce aux optimisations Senseye Human Vision."
+      }
+    }
 ]';
 
 
