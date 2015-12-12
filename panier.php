b63b272a7eb91ac38b74ab43d593c67e057c9be6
Victorien Schweitzer <victorien-s@live.fr>
2015-12-13 00:55:00 +0100

    Ajout du script d'ajout au panier en PHP

--- panier.php
+++ panier.php
@@ -1,11 +1,30 @@
 <?php
 
 if (isset($_POST['id_article']) ) {
-	// ici mettre a jou le panier
+    $json_produit = file_get_contents('produit.json');
+    $decoded_produit = json_decode($json_produit);
+
+    $produit_to_add = null;
+    foreach ($decoded_produit as $key => $produit) {
+        if ($produit->produit->id == $_POST['id_article']) {
+            $produit_to_add = $produit;
+            break;
+        }
+    }
+    if ($produit_to_add !== null) {
+        $json_panier = file_get_contents('panier.json');
+        $decoded_panier = json_decode($json_panier);
+        array_push($decoded_panier, $produit);
+        $encoded_panier = json_encode($decoded_panier);
+        file_put_contents('panier.json', $encoded_panier);
+
+    } else {
+        die(json_encode(array('message' => 'ERROR', 'code' => 404)));
+    }
 
 
 } else {
-	$json_source = file_get_contents('panier.json');
+    $json_source = file_get_contents('panier.json');
 	echo $json_source;
 }
 
